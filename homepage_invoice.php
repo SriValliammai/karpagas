b2833ca290322e7e0331f40f11be1dfc756b0644
SriValliammai <85178501+SriValliammai@users.noreply.github.com>
2021-06-17 23:44:48 +0530

    Update homepage_invoice.php

--- homepage_invoice.php
+++ homepage_invoice.php
@@ -100,146 +100,72 @@
 						   </div>
 						   <?php 
 						    if(isset($_POST['submit'])){
-								$_POST['bill_count']=0;
-								$_POST['total']="0";
-								$dd = $_POST['date_load'];
-								$datesplit = explode('-', $dd);
-								$year = $datesplit[0];
-								$month = $datesplit[1];
-								$day = $datesplit[2];
-								$loaded_date = $day."-".$month."-".$year;
-								$_SESSION['date'] = $loaded_date;
-								$load_entry_date = date("Y/m/d");
-								$split_entry_date=explode('/', $load_entry_date);
-								$current_year = $split_entry_date[0];
-								$current_month = $split_entry_date[1];
-								$current_date = $split_entry_date[2];
-								if($month>=04){
-									$business_year= $year."-".($year+1);
-									$bill_number= 0;
-								}
-								else{
-									//$bill_number = 269;
-									$business_year = ($year-1)."-".$year;
-								}
-								if(strlen($_POST['conformation_no'])==0){
-									$_POST['conformation_no'] = NULL;
-								}
-								if(strlen($_POST['e_way_bill_no'])==0){
-									$_POST['e_way_bill_no'] = NULL;
-								}
-								if(strlen($_POST['truck_freight'])==0){
-									$_POST['truck_freight'] = 0;
-								}
+								$objbillinsert = new bill_details();
+								if(empty($_POST['conformation_no'])){$_POST['conformation_no'] = NULL;}
+								if(empty($_POST['e_way_bill_no'])){$_POST['e_way_bill_no'] = NULL;}
+								if(empty($_POST['truck_freight'])){$_POST['truck_freight'] = 0;}
 								if($_POST['CGST']==""){$_POST['CGST']=0;}
 								if($_POST['SGST']==""){$_POST['SGST']=0;}
 								if($_POST['IGST']==""){$_POST['IGST']=0;}
-
-								
-								$last_bill_no="SELECT * FROM bill_details WHERE bill_no=(SELECT max(bill_no) FROM bill_details WHERE business_year='$business_year')";
-								
-
-								$bill_no_result = mysqli_query($conn,$last_bill_no);
-									while($row=mysqli_fetch_assoc($bill_no_result)){
-										$bill_number=$row["bill_no"];
-									}
-									++$bill_number;
 								$vehicleno=strtoupper($_POST['vehicle_no']);
-								$bill_add_details="INSERT INTO bill_details VALUES('$_POST[bill_count]','$bill_number','$_POST[company]','$_POST[conformation_no]','$_POST[e_way_bill_no]','$vehicleno','$_POST[transport_name]','$_POST[truck_freight]','$_POST[total]',($_POST[CGST]*10),($_POST[SGST]*10),($_POST[IGST]*10),'$_POST[date_load]', '$load_entry_date', '$business_year')";
-								if(!mysqli_query($conn,$bill_add_details)){
-									
+								$_POST['total']=0;
+								$load_entry_date = date("Y/m/d");
+								$get_company_id = mysqli_query($conn,"SELECT company_id FROM party_details WHERE company_name='$_POST[company]'");
+								if(!$get_company_id){
 									die('ERROR.'.mysqli_error());
 								}
-								
-								$_SESSION['companyname']= $_POST["company"];
-								$get_bill_no="SELECT * FROM bill_details WHERE bill_no=(SELECT max(bill_no) FROM bill_details WHERE business_year ='$business_year')";
-									/*if(!mysqli_query($conn,$get_bill_no)){
-										
-										die('ERROR.'.mysqli_error());
-									}
-									else{}*/
-									$result = mysqli_query($conn,$get_bill_no);
-									while($row=mysqli_fetch_assoc($result)){
-										$bill_count = $row["bill_count"];
-										$bill_number=$row["bill_no"];
-										$igst_number = $row["igst"];
-										$cgst_number = $row["cgst"];
-										$sgst_number = $row["sgst"];
+								else{
+									$company_id = mysqli_fetch_assoc($get_company_id);
+									$comp_id = $company_id['company_id'];
+								}
 
-									}
-									$_SESSION['recent_bill_count'] = $bill_count; 
-									$goods = $_POST['description_goods'];
-									$bags = $_POST['bags'];
-									$quantity = $_POST['quantity'];
-									$rate = $_POST['rate_per_unit'];
-									//hsn code is initialised as one later value updated
-									$hsncode = 1;
-									$sub_bilno=$bill_number;
-									$increment=1;
-									$_POST['is_deleted']="0";
-									foreach($goods as $key => $value){
-										if(($cgst_number!=0)||($sgst_number!=0)||($igst_number!=0)){
-											$load_d = "INSERT INTO load_details VALUES('$bill_count','$bill_number','$bill_count.$sub_bilno.$increment','".$value."','$hsncode','".$bags[$key]."','".round($quantity[$key], 3)."','".round($rate[$key], 5)."', round($quantity[$key], 3)*(round($rate[$key], 5)*1000),'$_POST[is_deleted]')";
-										}
-										else{
-										$load_d = "INSERT INTO load_details VALUES('$bill_count','$bill_number','$bill_count.$sub_bilno.$increment','".$value."','$hsncode','".$bags[$key]."','".$quantity[$key]."','".$rate[$key]."',$quantity[$key]*($rate[$key]*1000),'$_POST[is_deleted]')";
-										}
-										++$increment;
-										if(!mysqli_query($conn,$load_d))
-											{
-												die('ERROR.'.mysqli_error());
-											}
-									}
-									$get_material_name = mysqli_query($conn,"SELECT good_name FROM load_details WHERE bil_count= $bill_count");
-									$arrays = array();
-									while($row = mysqli_fetch_assoc($get_material_name)){
-										$arrays[] = $row;
-									}
-									//hsn code updated here
-									$count = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM load_details WHERE bil_count=$bill_count"));
-									$i = 0;
-									$z = 0;
-									for($x = 0; $x<=$count-1; $x++){
-										foreach($arrays[$x] as $goods){
-											$y[$i] = $goods;
-											$i++;
-										}
-										$get_hsncode=mysqli_query($conn,"SELECT * FROM goods_details WHERE goods_name= '$y[$z]'");
-										while($rows=mysqli_fetch_assoc($get_hsncode)){
-											$hsn_code = $rows["hsn_code"];
-											
-											mysqli_query($conn, "UPDATE load_details SET hsn_code='$hsn_code' WHERE good_name='$y[$z]'");
-										}
-									$z++;
-									}
-									$over_all_total=0;
-									$get_each_load_total=mysqli_query($conn, "SELECT current_total FROM load_details WHERE bil_count=$bill_count");
-									while($get_each_total=mysqli_fetch_assoc($get_each_load_total)){
-										$over_all_total = $over_all_total+$get_each_total['current_total'];
-									}
-									$get_truck_freight = mysqli_query($conn,"SELECT * FROM bill_details WHERE bill_count=$bill_count");
-									while($truck_freight=mysqli_fetch_assoc($get_truck_freight)){
-										$over_all_total -= $truck_freight["truck_freight"];
-									}
-									$total = round($over_all_total);
+								$objbillinsert -> billInsert($_POST['company'],$comp_id,$_POST['conformation_no'],$_POST['e_way_bill_no'],$vehicleno,$_POST['transport_name'],$_POST['truck_freight'],$_POST['total'],($_POST['CGST']*10),($_POST['SGST']*10),($_POST['IGST']*10),$_POST['date_load']);
+						    }
+class bill_details{
+       function billInsert($company,$compid,$conformation_no,$eway_bill,$vehicle_no,$transport_name,$truck_freigt,$total,$cgst,$sgst,$igst,$date){
+            include '../connection.php';
+            $entrydate = date("Y/m/d");
+            $split_entry_date=explode('/', $entrydate);
+            $current_year = $split_entry_date[0];
+            $current_month = $split_entry_date[1];
+            $current_date = $split_entry_date[2];
 
-									if(($cgst_number!=0)||($sgst_number!=0)||($igst_number!=0)){
-										
-										$cgst_number = $cgst_number/10;
-										$sgst_number = $sgst_number/10;
-										$igst_number = $igst_number/10;
-										$value_cgst = $total*($cgst_number/100);
-										$value_sgst = $total*($sgst_number/100);
-										$value_igst = $total*($igst_number/100);
-										$over_all_total += $value_cgst+$value_sgst+$value_igst;
-									}
+            if($current_month>=04){
+                $business_year= $current_year."-".($current_year+1);
+            }
+            else{
+                $business_year = ($current_year-1)."-".$current_year;
+            }
 
-									$grand_total = "UPDATE bill_details SET total=round($over_all_total) WHERE bill_count=$bill_count";
-									if(!mysqli_query($conn,$grand_total)){
-									   die('ERROR.'.mysqli_error());
-									}	
-							}
-						   ?>
+                $billno_query = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM bill_details WHERE business_year='$business_year'"));
+                if($billno_query > 0){
+                    $last_billno_query = mysqli_query($conn,"SELECT bill_no FROM bill_details WHERE bill_no=(SELECT max(bill_no) FROM bill_details WHERE business_year='$business_year')");
+                    $row=mysqli_fetch_assoc($last_billno_query);
+                     $bill_number=$row["bill_no"];
+                     $bill_no = ++$bill_number; 
+                }
+                elseif($billno_query==0){
+                    $bill_no = 1;
+                    // echo $bill_no;
+                }
+                else{
+                    die('ERROR.'.mysqli_error());
+                }
+            /*$sql = "INSERT INTO bill_details(bill_no,company,comp_id,conformation_no,e_way_bill,vehicle_no,transport_name,truck_freight,total,cgst,sgst,igst,date_of_load,bill_entry_date,business_year) VALUES($bill_no,'$company',$compid,'$conformation_no','$eway_bill','$vehicle_no','$transport_name',$truck_freigt,$total,$cgst,$sgst,$igst,'$date','$entrydate','$business_year')";*/
+            $bill_insert_query = mysqli_query($conn,"INSERT INTO bill_details(bill_no,company,comp_id,conformation_no,e_way_bill,vehicle_no,transport_name,truck_freight,total,cgst,sgst,igst,date_of_load,bill_entry_date,business_year) VALUES($bill_no,'$company',$compid,'$conformation_no','$eway_bill','$vehicle_no','$transport_name',$truck_freigt,$total,$cgst,$sgst,$igst,'$date','$entrydate','$business_year')");
+            if($bill_insert_query){
+                echo "insetted sucessfully";
+            }
+            else{
+                echo "error";
+            }
+
+
+       }
+       function billEdit(){
+
+       }
+   }
 						</form>
 					</div>
 				</div>
